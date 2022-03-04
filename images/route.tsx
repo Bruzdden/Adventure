88de14acea57d1bd14645739f057a163c9b9a7d6
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-03-04 18:34:47 +0100

    Add files via upload

--- images/route.tsx
+++ images/route.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="route" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="../../../Downloads/route.png" width="64" height="64"/>
+ <image source="route.png" width="64" height="64"/>
 </tileset>
