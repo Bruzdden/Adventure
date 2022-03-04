f8e4d3176384c3dab4f26e13d73f19c3e93574ef
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2022-03-04 18:23:39 +0100

    Add files via upload

---
+++ images/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="player" tilewidth="50" tileheight="80" tilecount="1" columns="1">
+ <image source="player.png" width="50" height="80"/>
+</tileset>
